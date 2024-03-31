a44877a3779140c779b9d26e910c5bdf5a3638d2
Alidevel <Alimerdanrahimov@gmail.com>
2024-03-31 16:56:04 +0000

    News

---
+++ retail_report/retail_report/doctype/restore_backup/restore_backup.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Alimerdan Rahimov and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Restore backup', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ retail_report/retail_report/doctype/restore_backup/test_restore_backup.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Alimerdan Rahimov and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRestorebackup(FrappeTestCase):
+	pass
