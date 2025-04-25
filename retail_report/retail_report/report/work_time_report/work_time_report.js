3e2cd40fedf4ba9cec6e98dc7c37ae080d075c02
Alimerdan Rahimov <94564048+AliDevel@users.noreply.github.com>
2025-04-25 21:23:21 +0200

    Create work_time_report.js
    Initial JS file created

---
+++ retail_report/retail_report/report/work_time_report/work_time_report.js
@@ -0,0 +1,20 @@
+// File: weekly_attendance_overview.js
+
+frappe.query_reports["Work Time Report"] = {
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_days(frappe.datetime.get_today(), -7),
+            "reqd": 1
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1
+        }
+    ]
+};
