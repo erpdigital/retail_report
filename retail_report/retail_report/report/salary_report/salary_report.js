bb35f51556cc4cce621fbaa6fdc61e8c56c4842e
Alimerdan Rahimov <94564048+AliDevel@users.noreply.github.com>
2025-05-16 22:55:31 +0200

    Update salary_report.js

--- retail_report/retail_report/report/salary_report/salary_report.js
+++ retail_report/retail_report/report/salary_report/salary_report.js
@@ -4,14 +4,14 @@ frappe.query_reports["Salary Report"] = {
             "fieldname": "from_date",
             "label": __("From Date"),
             "fieldtype": "Date",
-            "default": frappe.datetime.add_days(frappe.datetime.get_today(), -7),
+    
             "reqd": 1
         },
         {
             "fieldname": "to_date",
             "label": __("To Date"),
             "fieldtype": "Date",
-            "default": frappe.datetime.get_today(),
+     
             "reqd": 1
         }
     ]
