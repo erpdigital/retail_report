b2eb4c5378bdc878f429fedf3ff02c64f0a850f6
Alimerdan Rahimov <94564048+AliDevel@users.noreply.github.com>
2025-05-17 11:14:12 +0200

    Update salary_report.js

--- retail_report/retail_report/report/salary_report/salary_report.js
+++ retail_report/retail_report/report/salary_report/salary_report.js
@@ -1,4 +1,29 @@
 frappe.query_reports["Salary Report"] = {
+     onload: function(report) {
+        report.page.add_inner_button(__("Pay Payroll"), () => {
+            frappe.prompt([
+                {
+                    label: "Дата выплаты",
+                    fieldname: "payment_date",
+                    fieldtype: "Date",
+                    default: frappe.datetime.get_today(),
+                    reqd: 1
+                }
+            ], (values) => {
+                frappe.call({
+                    method: "create_payroll",
+                    args: {
+                        from_date: report.get_filter_value("from_date"),
+                        to_date: report.get_filter_value("to_date"),
+                        payment_date: values.payment_date
+                    },
+                    callback: function(r) {
+                        frappe.msgprint(__("Выплаты произведены."));
+                    }
+                });
+            }, __("Выплата зарплаты"));
+        });
+    },
     "filters": [
         {
             "fieldname": "from_date",
